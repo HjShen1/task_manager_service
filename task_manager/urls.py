a0d74e724b1b3c935556e2febf528af1fced9703
Huijia Shen <huijiashen@dhcp-vl2051-587.eduroam.cornell.edu>
2024-11-25 20:12:02 -0500

    Update urls.py with home url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to the Task Manager!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
+    path('', home_view, name='home'), 
 ]
